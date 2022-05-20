522d9ef451278ce0a26d0370f162fe68dc599cfa
Emilio <marqetm@gmail.com>
2022-05-19 22:54:07 -0500

    commit tree f8af063a679bdc4aec1fd98c3e9ae5f06ddb8c4e

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') {
+  return `This Saturday, I want to ${activity}!`
+}
+
+console.log(saturdayFun('study'));
+
+function mondayWork(activity = 'go to the office') {
+  return `This Monday, I will ${activity}.`;
+}
+
+console.log(mondayWork('work'));
+
+function wrapAdjective(visFlair = '*') {
+  return function(keyWord = 'special') {
+    return `You are ${visFlair}${keyWord}${visFlair}!`
+  }
+}
